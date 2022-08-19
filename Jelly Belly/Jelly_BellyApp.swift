2c6d997544f75396fcf8ad5484309b5a56ba881d
J Rogel-Salazar <jrogel.datascience@icloud.com>
2022-08-19 14:55:47 +0100

    Initial Commit

---
+++ Jelly Belly/Jelly_BellyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jelly_BellyApp.swift
+//  Jelly Belly
+//
+//  Created by J Rogel PhD on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Jelly_BellyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
