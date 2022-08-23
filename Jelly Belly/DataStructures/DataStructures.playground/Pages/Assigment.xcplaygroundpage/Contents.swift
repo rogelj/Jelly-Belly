0bc3f5f5461a0ea3d1d48dccbf3187e76f272199
J Rogel-Salazar <jrogel.datascience@icloud.com>
2022-08-23 12:13:47 +0100

    Creating some structures
    Including:
    - `cuisine`
    - `mealType`
    - `ingredients`

--- Jelly Belly/DataStructures/DataStructures.playground/Pages/Assigment.xcplaygroundpage/Contents.swift
+++ Jelly Belly/DataStructures/DataStructures.playground/Pages/Assigment.xcplaygroundpage/Contents.swift
@@ -1,8 +1,55 @@
 //: [Previous](@previous)
 //: ## Week 2 - Assignment
 
-import Foundation
+//: Let us start with some of the simpler components for the Dish class to be built later on.
+//: The dish class will comprise:
+//:   - name: String
+//:   - ingredients: Array
+//:   - calories: Float (calculated)
+//:   - cusine: String
+//:   - mealtype: String
+//:   - special: Bool (optional)
+//:   - dietary: Set (optional)
+//:   - cost: Float
 
-var greeting = "Hello, playground"
+
+//: ### `cuisine` - `Array`
+//: Looking at creating the array for `cuisine`. In this case we use an array as we are simply
+//: listing the different types of cuisine available. We can in principle add more.
+
+var cuisine: [String] = []
+cuisine.append("Italian")
+cuisine.append("Mexican")
+cuisine.append("American")
+cuisine.append("Japanese")
+cuisine.append("Chinese")
+cuisine.append("French")
+
+//: ### `mealType` - `Set`
+//: In this case we are using a set to differentiate different sections in the menu, such as starters,
+//: main dishes, beverages and desserts
+let mealType: Set = ["starter", "main", "beverage", "dessert"]
+
+//: ### `ingredients` - `Dictionary`
+//: The ingredients are stored in a dictionary that provides information about the calory content.
+//: At this stage I am not 100% convinced this is the most sustainable way to show this in the app. However, it
+//: gives me an opportunity to play with this data structure as this stage.
+var ingredients: [String: Float] = []
+ingredients["Fusilli"] = 450
+ingredients["Mozarella"] = 400
+ingredients["Pasta Sauce"] = 180
+ingredients["Pizza Base"] = 500
+ingredients["Parmesan"] = 300
+ingredients["Bread"] = 180
+ingredients["Garlic"] = 20
+ingredients["Olives"] = 50
+ingredients["Lemon"] = 100
+ingredients["Sugar"] = 250
+ingredients["Orange"] = 120
+ingredients["Chocolate"] = 200
+ingredients["Mascarpone"] = 450
+ingredients["Cream"] = 250
+ingredients["Coffee"] = 40
+
 
 //: [Next](@next)
