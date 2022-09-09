416dd009302a21bc4ea4cef83a48a9be5ba82f93
J Rogel <jrogel.datascience@icloud.com>
2022-09-09 10:31:19 +0100

    Setting up environment and moving data
    Setting up environment and moving test data to be reused

--- Jelly Belly/Models/DataStructures.swift
+++ Jelly Belly/Models/DataStructures.swift
@@ -37,6 +37,7 @@ struct Ingredients {
  
 */
 class Dish {
+    let id = UUID()
     let name: String
     var ingredients: [Ingredients]
     var cuisine: String
@@ -102,69 +103,8 @@ struct Order {
 
     init(loadTestData: Bool = false) {
         if loadTestData {
-            var testOrder = Order()
-            let fusilliArrabiata = Dish(name: "Fusilli Arrabiata",
-                                        ingredients: [Ingredients(ingredient: "Fusilli", portion: 2.0),
-                                                      Ingredients(ingredient: "Parmesan", portion: 1.0),
-                                                      Ingredients(ingredient: "Pasta Sauce", portion: 1.0)],
-                                        cuisine: DishParts.cuisine[0],
-                                        mealType: DishParts.mealType[1],
-                                        cost: 15.0,
-                                        special: true,
-                                        discountable: true)
-
-            let pizzaMargherita = Dish(name: "Pizza Margherita",
-                                       ingredients: [Ingredients(ingredient: "Pizza Base", portion: 1.0),
-                                                     Ingredients(ingredient: "Mozarella", portion: 2.0),
-                                                     Ingredients(ingredient: "Pasta Sauce", portion: 2.0)],
-                                       cuisine: DishParts.cuisine[0],
-                                       mealType: DishParts.mealType[1],
-                                       cost: 19.0,
-                                       dietary: DishParts.diet[0],
-                                       discountable: false)
-
-            let lemonade = Dish(name: "Lemonade",
-                                ingredients: [Ingredients(ingredient: "Lemon", portion: 3.0),
-                                              Ingredients(ingredient:"Sugar", portion: 1.0)],
-                                cuisine: DishParts.cuisine[2],
-                                mealType: DishParts.mealType[3],
-                                cost: 9.0)
-
-            let naranjada = Dish(name: "Naranjada",
-                                 ingredients: [Ingredients(ingredient: "Orange", portion: 3),
-                                               Ingredients(ingredient: "Sugar", portion: 0.5)],
-                                 cuisine: DishParts.cuisine[1],
-                                 mealType: DishParts.mealType[3],
-                                 cost: 12.0,
-                                 special: true)
-
-            let garlicBread = Dish(name: "Garlic Bread",
-                                   ingredients: [Ingredients(ingredient: "Garlic", portion: 1),
-                                                 Ingredients(ingredient: "Parmesan", portion: 2),
-                                                 Ingredients(ingredient: "Bread", portion: 2)],
-                                   cuisine: DishParts.cuisine[0],
-                                   mealType: DishParts.mealType[0],
-                                   cost: 9.0,
-                                   dietary: DishParts.diet[0],
-                                   discountable: true)
-
-            let tiramisu = Dish(name: "Tiramisu",
-                                ingredients: [Ingredients(ingredient: "Chocolate", portion: 1),
-                                              Ingredients(ingredient: "Cream", portion: 2),
-                                              Ingredients(ingredient: "Mascarpone", portion:  2),
-                                              Ingredients(ingredient: "Coffee", portion: 1)],
-                                cuisine: DishParts.cuisine[0],
-                                mealType: DishParts.mealType[2],
-                                cost: 14.0,
-                                discountable: false)
-
-            testOrder.addToOrder(dish: fusilliArrabiata)
-            testOrder.addToOrder(dish: pizzaMargherita)
-            testOrder.addToOrder(dish: lemonade)
-            testOrder.addToOrder(dish: naranjada)
-            testOrder.addToOrder(dish: garlicBread)
-            testOrder.addToOrder(dish: tiramisu)
-            order = testOrder.order
+            // Loading test data - now stored in `Dishes.swift`
+            order = dishes
         }
     }
 
---
+++ Jelly Belly/ViewControllers/MenuItemTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MenuItemTableViewController.swift
+//  Jelly Belly
+//
+//  Created by J Rogel PhD on 09/09/2022.
+//
+
+import UIKit
+
+class MenuItemTableViewController: UITableViewController {
+    
+    var menuItems = [Dish]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
