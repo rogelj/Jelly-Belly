512d23fb02391043de04101459957b3b20d3d790
J Rogel <jrogel.datascience@icloud.com>
2022-09-11 16:49:15 +0100

    Starting the Detailed View

---
+++ Jelly Belly/Views/MenuSwiftUIDetailedView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuSwiftUIDetailedView.swift
+//  Jelly Belly
+//
+//  Created by J Rogel PhD on 11/09/2022.
+//
+
+import SwiftUI
+
+struct MenuSwiftUIDetailedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuSwiftUIDetailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuSwiftUIDetailedView()
+    }
+}
