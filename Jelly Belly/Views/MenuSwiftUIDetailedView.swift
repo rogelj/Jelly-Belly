4c6413799ef6bdb7268dc9de170f03e24c63a926
J Rogel <jrogel.datascience@icloud.com>
2022-09-11 17:47:46 +0100

    Finishing Detail View

--- Jelly Belly/Views/MenuSwiftUIDetailedView.swift
+++ Jelly Belly/Views/MenuSwiftUIDetailedView.swift
@@ -7,14 +7,108 @@
 
 import SwiftUI
 
-struct MenuSwiftUIDetailedView: View {
+struct DishInformation: View {
+    var dish: Dish
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 3) {
+            HStack {
+                if dish.special != nil && dish.special == true {
+                    VStack(alignment: .leading){
+                        Text("Special")
+                            .foregroundColor(Color("Jelly"))
+                            .font(.headline)
+                    }
+                    Spacer()
+                }
+            }
+            HStack {
+                if dish.discountable != nil && dish.discountable == true {
+                    Text("Seasonal discount available")
+                        .foregroundColor(Color("Belly"))
+                        .font(.headline)
+                }
+                Spacer()
+            }
+            HStack(alignment: .top) {
+                VStack {
+                    DishItemText(text: "Ingredients:")
+                        .bold()
+                    ForEach(dish.ingredients.indices, id: \.self) {
+                        i in DishItemText(text: "\(dish.ingredients[i].ingredient)")
+                    }
+                }
+                VStack{
+                    DishItemText(text: "Cuisine:")
+                        .bold()
+                    DishItemText(text: dish.cuisine)
+                }
+            }
+            Spacer()
+                .frame(height: 5.0)
+            HStack(alignment: .top) {
+                VStack {
+                    DishItemText(text: "Calories:")
+                        .bold()
+                    DishItemText(text: "\(dish.getCalories())")
+                }
+                VStack {
+                    if let isDietary = dish.dietary {
+                        VStack{
+                            DishItemText(text: "Dietary Requirement:")
+                                .bold()
+                            DishItemText(text: isDietary)
+                            Spacer()
+                                .frame(height: 5.0)
+                        }
+                    }
+                }
+            }
+            HStack(alignment: .top) {
+                VStack {
+                    DishItemText(text: "Cost:")
+                        .bold()
+                    DishItemText(text: "£\(dish.cost)")
+                }
+                VStack {
+                    if dish.discountable != nil && dish.discountable == true {
+                        DishItemText(text: "Discounted Cost")
+                            .bold()
+                        DishItemText(text: "£\(dish.finalCost)")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct MenuSwiftUIDetailedView: View {
+    var dish: Dish
+    
+    var body: some View {
+        VStack {
+            Image(dish.name)
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color("Jelly"), lineWidth: 4)
+                )
+                .shadow(radius: 10)
+            Text(dish.name)
+                .font(.title)
+                .foregroundColor(Color("Belly"))
+            DishInformation(dish: dish)
+
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(Color("BackgroundColor"))
+        .cornerRadius(Constants.General.roudedRectCornerRadius)
+        .shadow(radius: 10, x: 5, y: 5)
+        .padding().navigationBarTitle(Text(dish.name), displayMode: .inline)
     }
 }
 
 struct MenuSwiftUIDetailedView_Previews: PreviewProvider {
     static var previews: some View {
-        MenuSwiftUIDetailedView()
+        MenuSwiftUIDetailedView(dish: testDish)
     }
 }
--- Jelly Belly/Views/RoundViews.swift
+++ Jelly Belly/Views/RoundViews.swift
@@ -106,82 +106,14 @@ struct DishView: View {
     var body: some View {
         VStack(spacing: 3) {
             HStack {
-                if dish.special != nil && dish.special == true {
-                    VStack(alignment: .leading){
-                        Text(dish.name.uppercased())
-                            .bold()
-                            .font(.title3)
-                        Text("Special")
-                            .foregroundColor(Color("Jelly"))
-                            .font(.headline)
-                    }
-                    Spacer()
-                } else {
-                    Text(dish.name.uppercased())
-                        .bold()
-                        .font(.title3)
-                    Spacer()
-                }
+                Text(dish.name.uppercased())
+                    .bold()
+                    .font(.title3)
+                Spacer()
                 Text(dish.mealType.1)
                     .font(.title)
             }
-            HStack {
-                if dish.discountable != nil && dish.discountable == true {
-                    Text("Seasonal discount available")
-                        .foregroundColor(Color("Belly"))
-                        .font(.headline)
-                }
-                Spacer()
-            }
-            HStack(alignment: .top) {
-                VStack {
-                    DishItemText(text: "Ingredients:")
-                        .bold()
-                    ForEach(dish.ingredients.indices, id: \.self) {
-                        i in DishItemText(text: "\(dish.ingredients[i].ingredient)")
-                    }
-                }
-                VStack{
-                    DishItemText(text: "Cuisine:")
-                        .bold()
-                    DishItemText(text: dish.cuisine)
-                }
-            }
-            Spacer()
-                .frame(height: 5.0)
-            HStack(alignment: .top) {
-                VStack {
-                    DishItemText(text: "Calories:")
-                        .bold()
-                    DishItemText(text: "\(dish.getCalories())")
-                }
-                VStack {
-                    if let isDietary = dish.dietary {
-                        VStack{
-                            DishItemText(text: "Dietary Requirement:")
-                                .bold()
-                            DishItemText(text: isDietary)
-                            Spacer()
-                                .frame(height: 5.0)
-                        }
-                    }
-                }
-            }
-            HStack(alignment: .top) {
-                VStack {
-                    DishItemText(text: "Cost:")
-                        .bold()
-                    DishItemText(text: "£\(dish.cost)")
-                }
-                VStack {
-                    if dish.discountable != nil && dish.discountable == true {
-                        DishItemText(text: "Discounted Cost")
-                            .bold()
-                        DishItemText(text: "£\(dish.finalCost)")
-                    }
-                }
-            }
-            
+            DishInformation(dish: dish)
         }
         .padding()
         .frame(maxWidth: .infinity)
@@ -235,8 +167,8 @@ struct RoundViews: View {
 
 struct RoundViews_Previews: PreviewProvider {
     static var previews: some View {
-        MenuRowView(dish: testDish)
         RoundViews()
+        MenuRowView(dish: testDish)
         RoundViews()
             .preferredColorScheme(.dark)
         RoundViews()
