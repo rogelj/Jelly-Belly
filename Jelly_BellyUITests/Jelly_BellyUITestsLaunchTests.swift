0c0a8b1f64b3831eee930fd2324b70939c146dfc
J Rogel <jrogel.datascience@icloud.com>
2022-10-20 18:02:29 +0100

    Adding UI tests

---
+++ Jelly_BellyUITests/Jelly_BellyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Jelly_BellyUITestsLaunchTests.swift
+//  Jelly_BellyUITests
+//
+//  Created by J Rogel PhD on 20/10/2022.
+//
+
+import XCTest
+
+final class Jelly_BellyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
