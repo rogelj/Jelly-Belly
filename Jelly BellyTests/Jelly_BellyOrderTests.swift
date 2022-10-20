76cb7b2cf5767e73817c65cca74a6902374281b3
J Rogel <jrogel.datascience@icloud.com>
2022-10-20 11:22:51 +0100

    Testing Dishes

---
+++ Jelly BellyTests/Jelly_BellyOrderTests.swift
@@ -0,0 +1,51 @@
+//
+//  Jelly_BellyOrderTests.swift
+//  Jelly BellyTests
+//
+//  Created by J Rogel PhD on 20/10/2022.
+//
+
+import XCTest
+@testable import Jelly_Belly
+
+final class Jelly_BellyOrderTests: XCTestCase {
+
+    var dishDisc: Dish!
+    var dishNoDisc: Dish!
+    var order: Order!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        dishDisc = Dish(name: "Fusilli Arrabiata",
+                        ingredients: [Ingredients(ingredient: "Fusilli", portion: 2.0),
+                                      Ingredients(ingredient:"Parmesan", portion: 1.0),
+                                      Ingredients(ingredient:"Pasta Sauce", portion: 1.0)],
+                        cuisine: DishParts.cuisine[0],
+                        mealCategory: .main,
+                        cost: 100.0,
+                        special: true,
+                        dietary: DishParts.diet[0],
+                        discountable: true,
+                        description: "Fussilli Arrabiata made with whole meal pasta and fresh tomatoes.")
+
+        dishNoDisc = Dish(name: "Fusilli Arrabiata",
+                          ingredients: [Ingredients(ingredient: "Fusilli", portion: 2.0),
+                                        Ingredients(ingredient:"Parmesan", portion: 1.0),
+                                        Ingredients(ingredient:"Pasta Sauce", portion: 1.0)],
+                          cuisine: DishParts.cuisine[0],
+                          mealCategory: .main,
+                          cost: 100.0,
+                          special: true,
+                          dietary: DishParts.diet[0],
+                          discountable: false,
+                          description: "Fussilli Arrabiata made with whole meal pasta and fresh tomatoes.")
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        dishDisc = nil
+        dishNoDisc = nil
+    }
+
+
+}
