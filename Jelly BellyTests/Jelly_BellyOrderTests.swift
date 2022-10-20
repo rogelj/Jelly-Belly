c3ca73321d03b2604ee824189474aec85012d688
J Rogel <jrogel.datascience@icloud.com>
2022-10-20 11:33:39 +0100

    Testing Orders

--- Jelly BellyTests/Jelly_BellyOrderTests.swift
+++ Jelly BellyTests/Jelly_BellyOrderTests.swift
@@ -12,7 +12,8 @@ final class Jelly_BellyOrderTests: XCTestCase {
 
     var dishDisc: Dish!
     var dishNoDisc: Dish!
-    var order: Order!
+    var myOrder: Order!
+    var newOrder: Order!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -39,12 +40,26 @@ final class Jelly_BellyOrderTests: XCTestCase {
                           dietary: DishParts.diet[0],
                           discountable: false,
                           description: "Fussilli Arrabiata made with whole meal pasta and fresh tomatoes.")
+
+        myOrder = Order(loadTestData: true)
+
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         dishDisc = nil
         dishNoDisc = nil
+        myOrder = nil
+    }
+
+    func test_NumDishesInOrder() {
+        let numDishes = myOrder.order.count
+        XCTAssertEqual(numDishes, 20)
+    }
+
+    func test_CreteOrder() {
+        newOrder = Order(order: [dishDisc, dishNoDisc])
+        XCTAssertNotNil(newOrder)
     }
 
 
