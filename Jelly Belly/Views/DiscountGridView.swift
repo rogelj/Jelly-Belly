b452de263c0e4d226fdf299cd54570d8fa51442d
J Rogel <jrogel.datascience@icloud.com>
2022-10-20 16:16:34 +0100

    Testing Requests

--- Jelly Belly/Views/DiscountGridView.swift
+++ Jelly Belly/Views/DiscountGridView.swift
@@ -1,89 +1,89 @@
+////
+////  DiscountGridView.swift
+////  Jelly Belly
+////
+////  Created by J Rogel PhD on 16/09/2022.
+////
 //
-//  DiscountGridView.swift
-//  Jelly Belly
+//import SwiftUI
 //
-//  Created by J Rogel PhD on 16/09/2022.
+//struct DiscountGridView: View {m
 //
-
-import SwiftUI
-
-struct DiscountGridView: View {
-
-    var downloader: MenuItems
-
-//    @State var menuDishes = dishes
-    @ObservedObject var orderCaretaker: OrderCaretaker
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                HStack {
-                    BigText(text: "Jelly Belly Discounts")
-                        .padding()
-                    Spacer()
-                    RoundLogoView(imageSize: Constants.Logo.logoViewSizeTiny )
-                        .padding()
-                    
-                }
-                ScrollView {
-                    ScrollViewReader { scrollProxy in
-                        LazyVGrid(
-                            columns: [.init(.adaptive(minimum: 150))]
-                        ) {
-                            ForEach(DishParts.MealCategory.allCases, id: \.self) { category in
-                                Section(header: MenuHeaderView(title: category.rawValue)) {
-                                    ForEach(self.getDiscountDishes(by: category)) { dish in
-                                        NavigationLink(destination: MenuDetailedView(dish: dish, orderCaretaker: orderCaretaker)) {
-                                            DishImage(dishName: dish.name)
-                                                .padding(.leading)
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    func getDiscountDishes(by mealCategory: DishParts.MealCategory) -> [Dish] {
-        downloader.myMenuDishes.filter { dish in
-            dish.mealCategory == mealCategory && dish.discountable == true
-        }
-    }
-}
-
-struct DishImage: View {
-    var dishName: String
-    
-    var body: some View {
-        VStack{
-            GeometryReader { proxy in
-                ZStack(alignment: .topTrailing) {
-                    DishCircle(dishName: dishName)
-                    Image(systemName: "exclamationmark.bubble.fill")
-                        .font(.largeTitle)
-                        .foregroundColor(Color("Belly"))
-                        .background(Color.white.opacity(0.1))
-                        .frame(width: proxy.size.width / 4, height: proxy.size.height / 5)
-                        .padding(proxy.size.width / 30)
-                }
-            }
-            .frame(width: 100, height: 100)
-            Text(dishName)
-                .font(.body)
-                .foregroundColor(Color("Belly"))
-        }
-    }
-}
-
-struct DiscountGridView_Previews: PreviewProvider {
-    static private var downloader = MenuItems()
-    static private var orderCaretaker = OrderCaretaker()
-    
-    static var previews: some View {
-        DiscountGridView(downloader: downloader, orderCaretaker: orderCaretaker)
-        DiscountGridView(downloader: downloader, orderCaretaker: orderCaretaker)
-            .preferredColorScheme(.dark)
-    }
-}
+//    var downloader: MenuItems
+//
+////    @State var menuDishes = dishes
+//    @ObservedObject var orderCaretaker: OrderCaretaker
+//
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                HStack {
+//                    BigText(text: "Jelly Belly Discounts")
+//                        .padding()
+//                    Spacer()
+//                    RoundLogoView(imageSize: Constants.Logo.logoViewSizeTiny )
+//                        .padding()
+//
+//                }
+//                ScrollView {
+//                    ScrollViewReader { scrollProxy in
+//                        LazyVGrid(
+//                            columns: [.init(.adaptive(minimum: 150))]
+//                        ) {
+//                            ForEach(DishParts.MealCategory.allCases, id: \.self) { category in
+//                                Section(header: MenuHeaderView(title: category.rawValue)) {
+//                                    ForEach(self.getDiscountDishes(by: category)) { dish in
+//                                        NavigationLink(destination: MenuDetailedView(dish: dish, orderCaretaker: orderCaretaker)) {
+//                                            DishImage(dishName: dish.name)
+//                                                .padding(.leading)
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    func getDiscountDishes(by mealCategory: DishParts.MealCategory) -> [Dish] {
+//        downloader.myMenuDishes.filter { dish in
+//            dish.mealCategory == mealCategory && dish.discountable == true
+//        }
+//    }
+//}
+//
+//struct DishImage: View {
+//    var dishName: String
+//
+//    var body: some View {
+//        VStack{
+//            GeometryReader { proxy in
+//                ZStack(alignment: .topTrailing) {
+//                    DishCircle(dishName: dishName)
+//                    Image(systemName: "exclamationmark.bubble.fill")
+//                        .font(.largeTitle)
+//                        .foregroundColor(Color("Belly"))
+//                        .background(Color.white.opacity(0.1))
+//                        .frame(width: proxy.size.width / 4, height: proxy.size.height / 5)
+//                        .padding(proxy.size.width / 30)
+//                }
+//            }
+//            .frame(width: 100, height: 100)
+//            Text(dishName)
+//                .font(.body)
+//                .foregroundColor(Color("Belly"))
+//        }
+//    }
+//}
+//
+//struct DiscountGridView_Previews: PreviewProvider {
+//    static private var downloader = MenuItems()
+//    static private var orderCaretaker = OrderCaretaker()
+//
+//    static var previews: some View {
+//        DiscountGridView(downloader: downloader, orderCaretaker: orderCaretaker)
+//        DiscountGridView(downloader: downloader, orderCaretaker: orderCaretaker)
+//            .preferredColorScheme(.dark)
+//    }
+//}
---
+++ Jelly BellyTests/Jelly_BellyAsynchTests.swift
@@ -0,0 +1,165 @@
+//
+//  Jelly_BellyAsynchTests.swift
+//  Jelly BellyTests
+//
+//  Created by J Rogel PhD on 20/10/2022.
+//
+
+import XCTest
+@testable import Jelly_Belly
+
+final class Jelly_BellyAsynchTests: XCTestCase {
+
+        let timeout: TimeInterval = 2
+        let timeout10: TimeInterval = 10
+        var expectation: XCTestExpectation!
+
+        var myMenu = MenuItems()
+
+        override func setUp() {
+            expectation = expectation(description: "Server responds in reasonable time")
+        }
+
+        func test_noServerResponse() {
+            let url = URL(string: "norestaurant")!
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                defer { self.expectation.fulfill() }
+
+                XCTAssertNil(data)
+                XCTAssertNil(response)
+                XCTAssertNotNil(error)
+            }
+            .resume()
+
+            waitForExpectations(timeout: timeout)
+        }
+
+        func test_decodeDish() {
+            let url = URL(string: "http://foodbukka.herokuapp.com/api/v1/menu")!
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                defer { self.expectation.fulfill() }
+
+                XCTAssertNil(error)
+
+                do {
+                    let response = try XCTUnwrap(response as? HTTPURLResponse)
+                    XCTAssertEqual(response.statusCode, 200)
+
+                    let data = try XCTUnwrap(data)
+                    XCTAssertNoThrow(
+                        try JSONDecoder().decode(MenuItems.FoodBukkaMenu.self, from: data)
+                    )
+                } catch { }
+            }
+            .resume()
+
+            waitForExpectations(timeout: timeout10)
+        }
+
+        func test_decodeOddDish() {
+            struct OddDish: Decodable {
+                let id: String
+                let name: String
+                let region : String
+            }
+            let url = URL(string: "http://foodbukka.herokuapp.com/api/v1/menu")!
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                defer { self.expectation.fulfill() }
+
+                XCTAssertNil(error)
+                XCTAssertEqual(
+                    (response as? HTTPURLResponse)?.statusCode,
+                    200
+                )
+
+                do {
+                    _ = try JSONDecoder().decode(
+                        MenuItems.FoodBukkaMenu.self,
+                        from: try XCTUnwrap(data)
+                    )
+                } catch {
+                    switch error {
+                    case DecodingError.keyNotFound(let key, _):
+                        XCTAssertEqual(key.stringValue, "region")
+                    default:
+                        XCTFail("\(error)")
+                    }
+                }
+            }
+            .resume()
+
+            waitForExpectations(timeout: timeout)
+        }
+
+        func test_404() {
+            let url = URL(string: "https://jrogel.com/mine")!
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                defer { self.expectation.fulfill() }
+
+                XCTAssertNil(error)
+                do {
+                    let response = try XCTUnwrap(response as? HTTPURLResponse)
+                    XCTAssertEqual(response.statusCode, 404)
+
+                    let data = try XCTUnwrap(data)
+                    XCTAssertThrowsError(
+                        try JSONDecoder().decode([Dish].self, from: data)
+                    ) { error in
+                        guard case DecodingError.dataCorrupted = error else {
+                            XCTFail("\(error)")
+                            return
+                        }
+                    }
+                } catch { }
+            }
+            .resume()
+
+            waitForExpectations(timeout: timeout10)
+        }
+
+//        func test_client() throws {
+//            struct FakeDataTestMaker: DataTaskMaker {
+//                static let dummyURL = URL(string: "dummy")!
+//
+//                init() throws {
+//                    let testBundle = Bundle(for: AsynchronousTestCase.self)
+//                    let url = try XCTUnwrap(
+//                        testBundle.url(forResource: "dogs", withExtension: "json")
+//                    )
+//                    data = try .init(contentsOf: url)
+//                }
+//                let data: Data
+//
+//                func dataTask(
+//                    with _: URL,
+//                    completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void)
+//                -> URLSessionDataTask {
+//                    completionHandler(
+//                        data,
+//                        HTTPURLResponse(
+//                            url: Self.dummyURL,
+//                            statusCode: 200,
+//                            httpVersion: nil,
+//                            headerFields: nil
+//                        ),
+//                        nil
+//                    )
+//
+//                    return URLSession(configuration: .default).dataTask(with: Self.dummyURL)
+//                }
+//            }
+//
+//            _ = DogPatchClient(
+//                baseURL: FakeDataTestMaker.dummyURL,
+//                session: try FakeDataTestMaker(), responseQueue: nil
+//            ).getDogs { dogs, error in
+//                defer { self.expectation.fulfill() }
+//
+//                XCTAssertEqual(dogs?.count, 0)
+//                XCTAssertNil(error)
+//            }
+//
+//            waitForExpectations(timeout: timeout)
+//        }
+    }
+
